944951174896c700404fd5b67151ed6b93332cae
IgLemp <87860192+IgLemp@users.noreply.github.com>
2023-06-22 23:45:09 +0200

    Vectored algorythm

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,7 @@ const Timer = std.time.Timer;
 const print = std.debug.print;
 const assert = std.debug.assert;
 
-const DIMESIONS = [_]u8{20, 40};
+const DIMESIONS = [_]u16{200, 400};
 const SIZE = DIMESIONS[0] * DIMESIONS[1];
 const SHOW_BOARD_STATE = false;
 
@@ -39,7 +39,6 @@ pub fn main() !void {
     var out_board = [_][DIMESIONS[1]]bool{[_]bool{false} ** DIMESIONS[1]} ** DIMESIONS[0];
 
     // =======================
-    print("Read time\n", .{});
     timer.reset();
     for (0..DIMESIONS[0] - 1) |o| {
         for (0..DIMESIONS[1] - 1) |p| {
@@ -51,35 +50,42 @@ pub fn main() !void {
 
     // =======================
     timer.reset();
-    printBoard(out_board);
+    if (SHOW_BOARD_STATE) { printBoard(out_board); }
     print_time = timer.lap();
-    print("read time:  {d}ms\n", .{read_time  / 1000});
-    print("print time: {d}ms\n", .{print_time / 1000});
+    print("read time:  {d}ms\n", .{read_time / 1000});
+    if (SHOW_BOARD_STATE) { print("print time: {d}ms\n", .{print_time / 1000}); } else { print("print time: N/A\n", .{}); }
     // =======================
     
 
     print("\n", .{});
-    const IterTimes = struct { list: u64 = 0, };
+    const IterTimes = struct { list: u64 = 0, vector: u64 = 0, };
     var iter_times: IterTimes = .{};
     
     // =======================
     timer.reset();
     gol.nextIterationList();
     iter_times.list = timer.lap();
-    comptime { if (SHOW_BOARD_STATE) { printBoard(gol.cells); } }
+    if (SHOW_BOARD_STATE) { printBoard(gol.cells); }
     print("list iter_time: {d}ms\n", .{iter_times.list / 1000});
+    if (SHOW_BOARD_STATE) { print("\n", .{}); }
     // =======================
 
 
+    // =======================
+    timer.reset();
     gol.nextIterationVector();
+    iter_times.vector = timer.lap();
+    if (SHOW_BOARD_STATE) { printBoard(gol.cells); }
+    print("vector iter_time: {d}ms\n", .{iter_times.vector / 1000});
+    // =======================
 }
 
-inline fn printBoard(board: [DIMESIONS[0]][DIMESIONS[1]]bool) void {
+inline fn printBoard(board: anytype) void {
     print("board state: \n", .{});
     print("=" ** DIMESIONS[1] ++ "\n", .{});
-    for (0..board.len) |i| {
-        for (0..board[0].len) |j| {
-            print("{s}", .{ if (board[i][j]) "X" else "."});
+    for (0..DIMESIONS[0]) |i| {
+        for (0..DIMESIONS[1]) |j| {
+            print("{s}", .{ if (@bitCast(bool, board[i][j])) "X" else "."});
         }
         print("\n", .{});
     }
@@ -116,7 +122,7 @@ fn GameOfLife(comptime x: usize, comptime y: usize) type {
             // TODO: make it use vectors
 
             // calculating number of living neighbours
-            inline for (1..x - 2) |i| {
+            for (1..x - 2) |i| {
                 for (1..y - 2) |j| {
                     // horizontal and vertical shifts
                     neighbours[i - 1][j] += self.cells[i][j]; // left  shift
@@ -133,57 +139,89 @@ fn GameOfLife(comptime x: usize, comptime y: usize) type {
             }
 
             // switching cells
-            inline for (0..x - 1) |i| {
+            for (0..x - 1) |i| {
                 for (0..y - 1) |j| {
                     var cell_neighbours = neighbours[i][j];
 
                     // 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation
-                    if (cell_neighbours < 2 and self.cells[i][j] == 1) { self.cells[i][j] = 0; }
+                    if (cell_neighbours < 2 and @bitCast(bool, self.cells[i][j]) == true) { self.cells[i][j] = 0; }
 
                     // 2. Any live cell with two or three live neighbours lives on to the next generation.
                     // NO-OP
 
                     // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-                    if (cell_neighbours > 3 and self.cells[i][j] == 1) { self.cells[i][j] = 0; }
+                    if (cell_neighbours > 3 and @bitCast(bool, self.cells[i][j]) == true) { self.cells[i][j] = 0; }
 
                     // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
-                    if (cell_neighbours == 3 and self.cells[i][j] == 0) { self.cells[i][j] = 0; }
+                    if (cell_neighbours == 3 and @bitCast(bool, self.cells[i][j]) == false) { self.cells[i][j] = 1; }
                 }
             }
         }
 
         pub fn nextIterationVector(self: *Self) void {
-            // rotation bitmasks fo SIMD operations
-            comptime var row_left_rot:  @Vector(DIMESIONS[0], u16) = undefined;
-            comptime var row_right_rot: @Vector(DIMESIONS[0], u16) = undefined;
-            comptime var col_left_rot:  @Vector(DIMESIONS[1], u16) = undefined;
-            comptime var col_right_rot: @Vector(DIMESIONS[1], u16) = undefined;
-
-            // row left rotation vector
-            row_left_rot[DIMESIONS[0] - 1] = 0;
-            inline for (0..DIMESIONS[0] - 1) |i| { row_left_rot[i] = i + 1; }
-
-            // row right rotation vector
-            row_right_rot[0] = DIMESIONS[0] - 1;
-            inline for (1..DIMESIONS[0]) |i| { row_right_rot[i] = i - 1; }
+            comptime var col_left_rot:  @Vector(y, u16) = undefined;
+            comptime var col_right_rot: @Vector(y, u16) = undefined;
 
             // col left rotation vector
-            col_left_rot[DIMESIONS[1] - 1] = 0;
-            inline for (0..DIMESIONS[1] - 1) |i| { col_left_rot[i] = i + 1; }
+            col_left_rot[y - 1] = 0;
+            @setEvalBranchQuota(100000);
+            inline for (0..y - 1) |i| { col_left_rot[i] = i + 1; }
 
             // col right rotation vector
-            col_right_rot[0] = DIMESIONS[1] - 1;
-            inline for (1..DIMESIONS[1]) |i| { col_right_rot[i] = i - 1; }
+            col_right_rot[0] = y - 1;
+            @setEvalBranchQuota(100000);
+            inline for (1..y) |i| { col_right_rot[i] = i - 1; }
         
-
+            // add iteration
             self.iteration += 1;
-            var neighbours = [_]@Vector(DIMESIONS[1], u4){} ** x;
-            _ = neighbours;
+            var neighbours = [_]@Vector(y, u4){@splat(y, @as(u1, 0))} ** x;
 
-            print("\n{any}", .{row_left_rot});
-            print("\n{any}", .{row_right_rot});
-            print("\n{any}", .{col_left_rot});
-            print("\n{any}", .{col_right_rot});
+            // column up and down rotations
+            for (&neighbours, self.cells) |*n_column, c_column| {
+                n_column.* += @shuffle(u4, c_column, undefined, col_left_rot);
+                n_column.* += @shuffle(u4, c_column, undefined, col_right_rot);
+            }
+
+            // row left and light rotations
+            for (1..x) |i| {
+                neighbours[i - 1] += self.cells[i];
+                neighbours[x - 1] += self.cells[0];
+            }
+            
+            for (0..x - 1) |i| {
+                neighbours[i + 1] += self.cells[i];
+                neighbours[0] += self.cells[x - 1];
+            }
+
+            // row column left up and left down rotations
+            for (1..x) |i| {
+                neighbours[i - 1] += @shuffle(u4, self.cells[i], undefined, col_left_rot);
+                neighbours[x - 1] += @shuffle(u4, self.cells[0], undefined, col_left_rot);
+                neighbours[i - 1] += @shuffle(u4, self.cells[i], undefined, col_right_rot);
+                neighbours[x - 1] += @shuffle(u4, self.cells[0], undefined, col_right_rot);
+            }
+
+            for (0..x - 1) |i| {
+                neighbours[i + 1] +=  @shuffle(u4, self.cells[i], undefined, col_left_rot);
+                neighbours[0] +=  @shuffle(u4, self.cells[x - 1], undefined, col_left_rot);
+                neighbours[i + 1] +=  @shuffle(u4, self.cells[i], undefined, col_right_rot);
+                neighbours[0] +=  @shuffle(u4, self.cells[x - 1], undefined, col_right_rot);
+            }
+
+            // game of life algorythm
+            for (&neighbours, &self.cells) |*n_list, *c_list| {
+                // 1. Any live cell with fewer than two live neighbours dies, as if by underpopulation
+                c_list.* ^= (c_list.* & @bitCast(@Vector(y, u1), n_list.* < @splat(y, @as(u4, 2))));
+                
+                // 2. Any live cell with two or three live neighbours lives on to the next generation.
+                // NO-OP
+                
+                // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+                c_list.* ^= (c_list.* & @bitCast(@Vector(y, u1), n_list.* > @splat(y, @as(u4, 3))));
+
+                // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+                c_list.* |= (~c_list.* & @bitCast(@Vector(y, u1), n_list.* == @splat(y, @as(u4, 3))));
+            }
         }
     };
 }
